344706b66cdb33e03bc2015d7eef3891a615f3f3
Vitor Emanuel <veslima3@gmail.com>
2019-10-13 19:25:50 -0300

    Add files via upload

---
+++ PHP/operacoes.php
@@ -0,0 +1,18 @@
+<?php
+
+$idade = 30;
+
+$idadeFimDeCurso = $idade + 3;
+
+echo $idadeFimDeCurso;
+
+// operacoes
+
+$soma = 2+2;
+$subtracao = 3-2;
+$multiplicacao = 3*3;
+$divisao = 2/2;
+$elevar = 2**3;
+$resto = 9%4;
+
+echo $soma;
