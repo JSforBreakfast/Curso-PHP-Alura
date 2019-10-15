ae11678e960e7694ef4e4d328e7ce58dfe1dc24c
Vitor Emanuel <veslima3@gmail.com>
2019-10-15 01:32:11 -0300

    Add files via upload

---
+++ PHP/strings.php
@@ -0,0 +1,5 @@
+<?php
+
+$dia = 16;
+// com aspas duplas o php reconhece variáveis dentro das strings, mas com aspas simples é necessário utilizar pontos para separar o conteúdo
+echo "Hoje é dia $dia de Outubro";
