17b61e809610ecb4417ba2f7858577e6c69e350c
Vitor Emanuel <veslima3@gmail.com>
2019-10-15 02:01:46 -0300

    Update strings.php

--- PHP/strings.php
+++ PHP/strings.php
@@ -2,5 +2,5 @@
 
 $dia = 16;
 // com aspas duplas o php reconhece variáveis dentro das strings, mas com aspas simples é necessário utilizar pontos para separar o conteúdo
-// echo 'Hoje é dia . $dia . de outubro';
+// echo 'Hoje é dia' . $dia . 'de outubro';
 echo "Hoje é dia $dia de Outubro";
