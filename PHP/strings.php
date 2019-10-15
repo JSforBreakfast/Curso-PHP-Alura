858a57fb7a793bfeebdf6c710118c62717ee9d9c
Vitor Emanuel <veslima3@gmail.com>
2019-10-15 13:02:04 -0300

    . PHP_EOL

--- PHP/strings.php
+++ PHP/strings.php
@@ -6,4 +6,4 @@ echo "Hoje é dia $dia de Outubro.\n";
 
 echo "\tBrooklyn nine-nine is the best underrated comedy show out there.". PHP_EOL;
 
-echo "I just need to add another line to test PHP_EOL";
+echo "I just needed to add another line to test PHP_EOL, but that last statement is absolutely true... nine-nine!!!";
