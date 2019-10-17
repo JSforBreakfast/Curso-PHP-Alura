1882bbaf0ebffbcc89b14e00dca041321afba946
Vitor Emanuel <veslima3@gmail.com>
2019-10-17 02:21:06 -0300

    else ifs

--- PHP/decisoes.php
+++ PHP/decisoes.php
@@ -2,12 +2,16 @@
 
 $idade = 17;
 
+$numPessoas = 2;
+
 //$nome = "Vitor";
 
-echo "Você só pode entrar se tiver 18 anos." . PHP_EOL;
+echo "Você só pode entrar se tiver 18 anos ou se tiver 16 e estiver acompanhado." . PHP_EOL;
 
 if ($idade >= 18 /*&& $nome == "Vitor"*/){
     echo "Você tem $idade anos. Pode entrar.";
+} else if ($idade >= 16 && $numPessoas > 1){
+    echo "Você tem $idade, mas está acompanhado. Pode entrar.";
 } else {
     echo "Não pode entrar.";
 }
