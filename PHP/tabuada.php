7378e7b557fd315166056bdc163d1f145ad19015
Vitor Emanuel <veslima3@gmail.com>
2019-10-18 01:33:44 -0300

    Update tabuada.php

--- PHP/tabuada.php
+++ PHP/tabuada.php
@@ -3,6 +3,6 @@
 
 for($multi = 1; $multi <= 10; $multi++){
    for($num = 1; $num <= 10; $num++){
-    echo "$num", "x", "$multi", "=", $num*$multi . PHP_EOL;   
+    echo "$num", " x ", "$multi", " = ", $num*$multi . PHP_EOL;   
    }
 }
