40b5e653635c8352020d8dde68ff4f756713d70b
Vitor Emanuel <veslima3@gmail.com>
2019-10-17 23:56:59 -0300

    while/do while/for
    https://www.php.net/manual/en/control-structures.while.php
    
    https://www.php.net/manual/en/control-structures.do.while.php
    
    https://www.php.net/manual/en/control-structures.for.php

--- PHP/repeticao.php
+++ PHP/repeticao.php
@@ -1,8 +1,12 @@
 <?php 
 
-$contador = 1;
+$contador = 0;
 
 while ($contador <= 9){
     echo "#$contador" . PHP_EOL;
     $contador = ++$contador;
+}
+
+for ($cont = 8; $cont >= 0; $cont--){
+    echo "#$cont". PHP_EOL;
 }
