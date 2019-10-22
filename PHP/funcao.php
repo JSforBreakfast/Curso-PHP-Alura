b941a79077cfeda45425008193ed8df0c57503e1
Vitor Emanuel <veslima3@gmail.com>
2019-10-22 03:27:04 -0300

    funcao 2.0
    Adicionar tipos de dados (array, float) nas funcções de saque e depósito.

--- PHP/funcao.php
+++ PHP/funcao.php
@@ -4,12 +4,22 @@ function exibeMensagem($mensagem){
     echo $mensagem . PHP_EOL;
 }
 
-function sacar($conta, $valorSacar) {
+function sacar(array $conta, float $valorSacar): array {
     if ($valorSacar > $conta['saldo']) {
         exibeMensagem("Saldo insuficiente para esta operação.");
     } else {
         $conta['saldo'] -= $valorSacar;
     }
+    return $conta;
+}
+
+function depositar(array $conta, float $valorDepositar): array {
+    if ($valorDepositar > 0) {
+        $conta['saldo'] += $valorDepositar;
+    } else {
+        exibeMensagem("Depósitos não podem ocasionar débito");
+    }
+
     return $conta;
 }
 
@@ -30,6 +40,8 @@ $contasCorrentes = [
 
 $contasCorrentes['321.456.987-78'] = sacar($contasCorrentes['321.456.987-78'], 500);
 $contasCorrentes['987.456.123-45'] = sacar($contasCorrentes['987.456.123-45'], 6000);
+
+$contasCorrentes['123.456.789-25'] = depositar($contasCorrentes['123.456.789-25'],900);
 
 foreach ($contasCorrentes as $cpf => $conta){
     exibeMensagem($cpf ." => ". $conta['titular'] . " seu saldo é de " . $conta['saldo'] . PHP_EOL);
