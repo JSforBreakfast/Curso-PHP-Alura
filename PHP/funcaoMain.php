7ccae8b811c2e64c9e8a116a9b2fad05e268bed8
Vitor Emanuel <veslima3@gmail.com>
2019-10-23 14:17:59 -0300

    aprendendo a usar '&' para referenciar variárveis

--- PHP/funcaoMain.php
+++ PHP/funcaoMain.php
@@ -1,10 +1,10 @@
 <?php
-//include não acusa erros de difitação no nome do arquivo imediatamente. O PHP vai tentar executar o programa apesar do erro no nome do aqruivo. 
+//include não acusa erros de digitação no nome do arquivo imediatamente. O PHP vai tentar executar o programa apesar do erro no nome do aqruivo. 
 //include "funcaoLink.php";
 
 //utilizando require, caso existam erros de digitação no nome do arquivo sendo chamado, a execução do programa para de imediato.
 //require_once é uma forma de evitar que o PHP tente fazer chamadas duplicadas de um arquivo. Caso isso acontecesse, o PHP acusaria o erro.
-require_once "funcao2.php";
+require_once "funcaoLink.php";
 
 $contasCorrentes = [
     '123.456.789-25' => [
@@ -25,6 +25,8 @@ $contasCorrentes['321.456.987-78'] = sacar($contasCorrentes['321.456.987-78'], 5
 $contasCorrentes['987.456.123-45'] = sacar($contasCorrentes['987.456.123-45'], 6000);
 
 $contasCorrentes['123.456.789-25'] = depositar($contasCorrentes['123.456.789-25'],900);
+
+titularMaiusculas($contasCorrentes['987.456.123-45']);
 
 foreach ($contasCorrentes as $cpf => $conta){
     exibeMensagem("$cpf {$conta['titular']} {$conta['saldo']}");
