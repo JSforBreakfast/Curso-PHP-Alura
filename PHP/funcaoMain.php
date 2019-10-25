b93eb321c8237c5a295c598790c6e0fd0ec53916
Vitor Emanuel <veslima3@gmail.com>
2019-10-25 13:14:55 -0300

    unset()

--- PHP/funcaoMain.php
+++ PHP/funcaoMain.php
@@ -21,10 +21,13 @@ $contasCorrentes = [
     ]
 ];
 
-$contasCorrentes['321.456.987-78'] = sacar($contasCorrentes['321.456.987-78'], 500);
+//$contasCorrentes['321.456.987-78'] = sacar($contasCorrentes['321.456.987-78'], 500);
 $contasCorrentes['987.456.123-45'] = sacar($contasCorrentes['987.456.123-45'], 6000);
 
 $contasCorrentes['123.456.789-25'] = depositar($contasCorrentes['123.456.789-25'],900);
+
+unset($contasCorrentes['321.456.987-78']);
+
 
 titularMaiusculas($contasCorrentes['987.456.123-45']);
 
