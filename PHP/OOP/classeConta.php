2e60a7d4c2a652b99c4c076c70ef1da7fbbacc87
Vitor Emanuel <veslima3@gmail.com>
2019-10-29 12:18:56 -0300

    oop php 1

--- PHP/OOP/classeConta.php
+++ PHP/OOP/classeConta.php
@@ -1,3 +1,21 @@
 <?php 
 
+require_once "contaCorrente.php";
+//se a variável for um objeto, não será possível mostrar no browser usando echo. Dá erro. É preciso utilizar var_dump();
+$contaMateus = new contaCorrente("Mateus","4565","3553-4",5000,00);
+$contaBruna = new contaCorrente("Bruna","4565","4563-4",7000,00);
+
+//$contaMateus->titular = "Mateus";
+//$contaMateus->agencia = "4565";
+//$contaMateus->numero = "3553-4";
+//$contaMateus->saldo = "5000,00";
+
+// $contaBruna->titular = "Bruna";
+// $contaBruna->agencia = "4565";
+// $contaBruna->numero = "4563-4";
+// $contaBruna->saldo = "7000,00";
+
+var_dump($contaMateus);
+var_dump($contaBruna);
+
 ?>
