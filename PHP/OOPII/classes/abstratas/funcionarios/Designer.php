0fe31913ad4f76aefa1fe8dc2510eb3b84575c07
Vitor Emanuel <veslima3@gmail.com>
2019-11-05 13:13:36 -0300

    polimorfismo - métodos com mesmo nome

--- PHP/OOPII/classes/abstratas/funcionarios/Designer.php
+++ PHP/OOPII/classes/abstratas/funcionarios/Designer.php
@@ -6,6 +6,16 @@ use OOPII\classes\abstratas\Funcionario;
 
 class Designer extends Funcionario{
 
+    public function getBonificacao(){
+
+        return $this->salario * 0.5;
+    }
+
+    public function aumentoSalario(){
+
+        $this->salario *= 1.3;
+    }
+     
 }
 
 ?>
