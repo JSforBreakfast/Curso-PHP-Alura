301b73441b482c219c3c7ca72bd3b867585df4b6
Vitor Emanuel <veslima3@gmail.com>
2019-11-05 11:19:13 -0300

    Encapsulando atributos com protected ou private

--- PHP/OOPII/classes/abstratas/Funcionario.php
+++ PHP/OOPII/classes/abstratas/Funcionario.php
@@ -8,7 +8,15 @@ class Funcionario{
 
     public $cpf;
 
-    public $salario;
+    private $salario;
+    //o salário somente poderá ser definido através do construtor
+    //se em vez de usar private fosse usado protect, o valor poderia ser alterado através dentro da classe
+    //utilizando private, o máximo que pode acontecer é ocorrer a criação de um outro atributo público sem alteração do valor do atributo privado
+    public function __construct($cpf, $salario){
+        
+        $this->cpf = $cpf;
+        $this->salario = $salario;
+    }
 
 }
 
