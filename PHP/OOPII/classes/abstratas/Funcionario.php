2764f7d6059fa19b3de03739dd1143fcd0494732
Vitor Emanuel <veslima3@gmail.com>
2019-11-05 11:27:12 -0300

    Encapsulando atributos com protected e private
     https://www.php.net/manual/en/language.oop5.visibility.php
     https://www.php.net/manual/en/language.oop5.inheritance.php

--- PHP/OOPII/classes/abstratas/Funcionario.php
+++ PHP/OOPII/classes/abstratas/Funcionario.php
@@ -9,6 +9,8 @@ class Funcionario{
     public $cpf;
 
     private $salario;
+    //https://www.php.net/manual/en/language.oop5.visibility.php
+    //https://www.php.net/manual/en/language.oop5.inheritance.php
     //o salário somente poderá ser definido através do construtor
     //se em vez de usar private fosse usado protect, o valor poderia ser alterado através dentro da classe
     //utilizando private, o máximo que pode acontecer é ocorrer a criação de um outro atributo público sem alteração do valor do atributo privado
