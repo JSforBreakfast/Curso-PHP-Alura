e392371a942a967844e69424f3e6f2fe7ddbf8b4
Vitor Emanuel <veslima3@gmail.com>
2019-11-07 12:38:25 -0300

    Update Diretor.php

--- PHP/OOPII/classes/abstratas/funcionarios/Diretor.php
+++ PHP/OOPII/classes/abstratas/funcionarios/Diretor.php
@@ -6,7 +6,11 @@ use OOPII\classes\abstratas\Funcionario;
 use OOPII\classes\abstratas\FuncionarioAutenticavel;
 
 class Diretor extends FuncionarioAutenticavel{
+    
+    public function getBonificacao(){
 
+        return $this->salario * 0.5; 
+    }
 }
 
 ?>
