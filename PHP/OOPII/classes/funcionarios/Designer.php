e4b7af508437138f8a0d2c3f227578fb241404c2
Vitor Emanuel <veslima3@gmail.com>
2019-11-07 12:52:21 -0300

    atualização classes abstratas

--- PHP/OOPII/classes/funcionarios/Designer.php
+++ PHP/OOPII/classes/funcionarios/Designer.php
@@ -1,15 +1,16 @@
-//<?php
+ 
+<?php
+namespace OOPII\classes\abstratas\funcionarios;
 
-//namespace OOPII\classes\funcionarios;
+use OOPII\classes\abstratas\Funcionario;
 
-//class Designer {
-
-//    public $nome;
-
-//    public $cpf;
-
-//    public $salario;
-
-//}
-
-//?>
+class Designer extends Funcionario{
+    public function getBonificacao(){
+        return $this->salario * 0.5;
+    }
+    public function aumentoSalario(){
+        $this->salario *= 1.3;
+    }
+     
+}
+?>
