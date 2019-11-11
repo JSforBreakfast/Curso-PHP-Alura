cb2c4f78b2065cfa0bd64c581391bcb280eb82f5
Vitor Emanuel <veslima3@gmail.com>
2019-11-11 00:15:20 -0300

    Update index.php

--- PHP/OOPII/index.php
+++ PHP/OOPII/index.php
@@ -6,20 +6,26 @@ require_once "autoload.php";
 use OOPII\classes\abstratas\funcionarios\Diretor;
 use OOPII\classes\abstratas\funcionarios\Designer;
 use OOPII\classes\abstratas\Funcionario;
+
 use OOPII\classes\sistemaInterno\GerenciadorBonificacao;
 
 $diretor = new Diretor("123.258.789-45", 15000.00);
+$diretor->senha = "000000";
+
 $designer = new Designer("147.963.456-99", 5000.00);
 
 
 $gerenciador = new GerenciadorBonificacao();
 
-$gerenciador->registrar($diretor);
+$gerenciador->AutentiqueAqui($diretor ,"000000");
 
-var_dump($gerenciador->getTotalBonificacao());
+//$gerenciador->registrar($diretor);
+
+//var_dump($gerenciador->getTotalBonificacao());
 
 var_dump($diretor);
 var_dump($designer);
+
 
 
 ?>
