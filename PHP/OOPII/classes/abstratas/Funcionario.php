482c9cb40e939bc55646d8bf084b426ad9770981
Vitor Emanuel <veslima3@gmail.com>
2019-11-11 10:31:58 -0300

    Update Funcionario.php

--- PHP/OOPII/classes/abstratas/Funcionario.php
+++ PHP/OOPII/classes/abstratas/Funcionario.php
@@ -8,11 +8,13 @@ abstract class Funcionario{
 
     public $cpf;
 
-    private $salario;
+   protected $salario;
+
+   const piso = 980.00;
     //o salário somente poderá ser definido através do construtor
     //se em vez de usar private fosse usado protect, o valor poderia ser alterado através dentro da classe
     //utilizando private, o máximo que pode acontecer é ocorrer a criação de um outro atributo público sem alteração do valor do atributo privado
-    public function __construct($cpf, $salario){
+   public function __construct($cpf, $salario = self::piso){
         
         $this->cpf = $cpf;
         $this->salario = $salario;
@@ -20,9 +22,9 @@ abstract class Funcionario{
 
    abstract public function getBonificacao();
 
-    public function aumentoSalario(){
+    final public function aumentoSalario(){
 
-        $this->salario *= 1.5;       
+        $this->salario *= 1.3;       
     }
 
 }
