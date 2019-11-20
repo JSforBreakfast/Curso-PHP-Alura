87c7f70e2128b580bcd947d822cbbd83524395a9
Vitor Emanuel <veslima3@gmail.com>
2019-11-20 13:36:05 -0300

    Update indexnota.php

--- PHP/Arrays/indexnota.php
+++ PHP/Arrays/indexnota.php
@@ -1,26 +1,24 @@
 <?php 
 
+require "calculadora.php";
+
 $notas = [9, 8, 7, 6, 10, 7];
 
 
-echo "<p>A nota de português é: $notas[0]\n</p>";
-echo "<p>A nota de matemática é: $notas[1]\n</p>";
-echo "<p>A nota de geografia é: $notas[2]\n</p>";
-echo "<p>A nota de história é: $notas[3]\n</p>";
-echo "<p>A nota de logica é: $notas[4]\n</p>";
-echo "<p>A nota de física é: $notas[5]\n</p>";
+//echo "<p>A nota de português é: $notas[0]\n</p>";
+//echo "<p>A nota de matemática é: $notas[1]\n</p>";
+//echo "<p>A nota de geografia é: $notas[2]\n</p>";
+//echo "<p>A nota de história é: $notas[3]\n</p>";
+//echo "<p>A nota de logica é: $notas[4]\n</p>";
+//echo "<p>A nota de física é: $notas[5]\n</p>";
 
+$calculadora = new Calculadora();
+$media = $calculadora->calculaMedia($notas);
 
-$quantNotas = sizeof($notas);
-
-$soma = 0;
-
-for($i = 0; $i < $quantNotas; $i++){
-    $soma += $notas[$i];
+if($media){
+    echo "A média é: $media.";
+}else{
+    echo "Não foi possível calcular a média.";
 }
-
-$media = $soma/$quantNotas;
-
-echo "<p>A média é: $media.\n</p>"
 
 ?>
