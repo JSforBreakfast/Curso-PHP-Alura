62f87a66706180a2fd107d21351d5b718b6c0b94
Vitor Emanuel <veslima3@gmail.com>
2019-11-20 12:52:24 -0300

    Update indexnota.php

--- PHP/Arrays/indexnota.php
+++ PHP/Arrays/indexnota.php
@@ -1,18 +1,25 @@
 <?php 
 
-$notaPortugues = 9;
-$notaMatematica = 8;
-$notaGeografia = 7;
-$notaHistoria = 6;
-$notaLogica = 10;
+$notas = [9, 8, 7, 6, 10, 7];
 
-echo "<p>a nota de português é: $notaPortugues\n</p>";
-echo "<p>a nota de matemática é: $notaMatematica\n</p>";
-echo "<p>a nota de geografia é: $notaGeografia\n</p>";
-echo "<p>a nota de história é: $notaHistoria\n</p>";
-echo "<p>a nota de logica é: $notaLogica\n</p>";
 
-$media = ($notaPortugues+$notaMatematica+$notaGeografia+$notaHistoria+$notaLogica)/5;
+echo "<p>A nota de português é: $notas[0]\n</p>";
+echo "<p>A nota de matemática é: $notas[1]\n</p>";
+echo "<p>A nota de geografia é: $notas[2]\n</p>";
+echo "<p>A nota de história é: $notas[3]\n</p>";
+echo "<p>A nota de logica é: $notas[4]\n</p>";
+echo "<p>A nota de física é: $notas[5]\n</p>";
+
+
+$quantNotas = sizeof($notas);
+
+$soma = 0;
+
+for($i = 0; $i < $quantNotas; $i++){
+    $soma += $notas[$i];
+}
+
+$media = $soma/$quantNotas;
 
 echo "<p>A média é: $media.\n</p>"
 
