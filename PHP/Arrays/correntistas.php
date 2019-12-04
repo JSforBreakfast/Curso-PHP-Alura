2f7a80defc78de29b6d20946f5eb2ce2c3a8f825
Vitor Emanuel <veslima3@gmail.com>
2019-12-04 17:25:52 -0300

    Update correntistas.php

--- PHP/Arrays/correntistas.php
+++ PHP/Arrays/correntistas.php
@@ -6,16 +6,15 @@ $correntistas_e_compras = [
     "Geofano",
      12,
     "Marilodia",
-     25,
     "Luizanine",
     "Luferaldo",
     "12"
     ];
 
+var_dump($correntistas_e_compras);
 
+arrayUtils::remover(12, $correntistas_e_compras);
 
 var_dump($correntistas_e_compras);
 
-arrayUtils::remover("Geofano", $correntistas_e_compras);
-
-var_dump($correntistas_e_compras);
+//https://www.php.net/manual/en/language.types.type-juggling.php
