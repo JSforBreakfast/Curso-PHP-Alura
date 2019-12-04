9af567817e537202e7f968283e5bdd13e2d4b53b
Vitor Emanuel <veslima3@gmail.com>
2019-12-04 17:12:57 -0300

    Update arrayUtils.php

--- PHP/Arrays/arrayUtils.php
+++ PHP/Arrays/arrayUtils.php
@@ -4,7 +4,12 @@ class ArrayUtils{
 
     public static function remover(string $elemento, array &$array)
     {
-    $posicao = array_search($elemento, $array);
-    unset($array[$posicao]);
+        $posicao = array_search($elemento, $array);
+        //é necessário utilizar a função is_int() para caso o index do array seja 0.
+        if (is_int($posicao)) {
+            unset($array[$posicao]);
+        }else{
+            echo "Não foi encontrado no array.";
+        }
     }
 }
