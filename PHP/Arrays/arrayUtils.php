d6e712df42b63f179b97091b4a69e84eb1305a71
Vitor Emanuel <veslima3@gmail.com>
2019-12-04 17:27:04 -0300

    Update arrayUtils.php

--- PHP/Arrays/arrayUtils.php
+++ PHP/Arrays/arrayUtils.php
@@ -1,8 +1,10 @@
-<?php
+<?php declare(strict_types=1);
+
+//https://dev.to/robdwaller/how-php-type-declarations-actually-work-1mm5
 
 class ArrayUtils{
 
-    public static function remover(string $elemento, array &$array)
+    public static function remover(int $elemento, array &$array)
     {
         $posicao = array_search($elemento, $array);
         //é necessário utilizar a função is_int() para caso o index do array seja 0.
